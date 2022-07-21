cfb8c829a8b39cb541b76660082fc25b3f0953c7
JacksonSM <jacksons.m2018@gmail.com>
2022-07-21 10:55:41 -0300

    Initial commit

---
+++ src/AspStore/AspStore.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspStore.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
